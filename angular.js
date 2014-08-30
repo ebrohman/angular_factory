28a5ec7d7cc0d93fda0618d624abb6b30773b987
ebrohman <ericbrohman@gmail.com>
2014-08-30 17:06:31 -0500

    First Commit
    Angular factory / resource.  Simple shopping cart.

---
+++ angular.js
@@ -0,0 +1,40 @@
+var boxes = angular.module('boxes', []);
+
+boxes.factory('Cart', function () {
+  return [];
+});
+
+boxes.controller('CatalogCtrl', function($scope, $http, Cart) {
+  $scope.boxes = [];
+  $scope.cart = Cart;
+
+  $http
+    .get('http://tsljs.herokuapp.com/boxes.json')
+
+    .success(function (results) {
+      $scope.boxes = results;
+    });
+
+  $scope.addToCart = function(e, box) {
+    e.preventDefault();
+    $scope.cart.push(box);
+  };
+});
+
+boxes.controller('CartCtrl', function ($scope, Cart) {
+  var baseShipping = 10;
+
+  $scope.cart = Cart;
+  $scope.shipping = baseShipping;
+
+  $scope.$watch('cart', function (newCart, oldCart) {
+    var subtotal = 0;
+    for(var i=0; i < newCart.length; i++) {
+      subtotal += newCart[i].price;
+    }
+
+    $scope.subtotal = subtotal;
+    $scope.shipping = subtotal > 20 ? 0 : baseShipping;
+  }, true);
+
+});
